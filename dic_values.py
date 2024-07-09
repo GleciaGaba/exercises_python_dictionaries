306715b9c5717a69f762f6a0ba3c8f0dd20f89be
GleciaGaba <gleciasf@hotmail.com>
2024-07-09 16:02:49 +0200

    update

---
+++ dic_values.py
@@ -0,0 +1,19 @@
+"""
+Pour itérer sur les valeurs, Python met à notre disposition la méthode values :
+
+"""
+
+
+d = {
+    'spam': 'ham',
+    'knight': 'lumberjack',
+    'bacon': 'sausage'
+}
+for value in d.values():
+    print(value)
+
+"""
+ham
+lumberjack
+sausage
+"""
---
+++ verifier_presence_cle_dans_un_dic.py
@@ -0,0 +1,14 @@
+"""
+Vérifier la présence d'une clé dans un dictionnaire
+Vous pouvez vérifier qu'une clé est présente dans un dictionnaire grâce à l'opérateur in :
+
+"""
+
+d = {
+    'spam': 'ham',
+    'knights': 'lumberjack',
+    'bacon': 'sausage'
+}
+
+print('bacon' in d)  # True
+print('ham' in d)  # False
